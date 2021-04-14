cfaa474e2f8acc875f16a3c6d3ac5a6c2c534846
MohammadAlkhat1 <>
2021-04-14 14:48:50 +0000

    update12

--- JS/lab04.js
+++ JS/lab04.js
@@ -1,1 +1,39 @@
-alert("hello");
+var smw=prompt('choose your favorite social network: facebook, twitter, linkedin, instagram ')
+var image = null;
+var link=null;
+alert('Hello in '+ smw+ 'World')
+
+if (smw='facebook')
+{
+  image='<img src="images/face1.png" alt="facebook" >'
+  link='<a href="https://web.facebook.com">www.facebook.com</a>'
+  document.write(image);
+  document.write(link);
+ 
+  
+}
+else if(smw='twitter')
+{
+  image='<img src="images/twitte.png" alt="twitter" >'
+  link='<a href="https://twitter.com/?lang=en">www.twitter.com</a>'
+  document.write(image);
+  document.write(link);
+}
+  
+else if(smw='linkedin')
+{
+  image='<img src="images/LinkedIn.png" alt="LinkedIn">'
+  link='<a href="https://www.linkedin.com">www.linkedin.com </a>'
+   document.write(image);
+  document.write(link);
+
+}
+else if(smw='instagram')
+{
+  image='<img src="images/insta1.jpg" alt="instagram">'
+  link='<a href="https://www.instagram.com">www.instagram.com </a>'
+ document.write(image);
+  document.write(link);
+}
+
+
